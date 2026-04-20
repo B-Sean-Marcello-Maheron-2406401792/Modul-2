63f80116fac84f537d21c6444fac89d556d9a265
unknown <seanmarcello836@gmail.com>
2026-04-20 14:17:45 +0700

    change directory

---
+++ Projects/hello/target/debug/deps/hello-54e026682740f9fb.d
@@ -0,0 +1,5 @@
+C:\Users\seanm\OneDrive\Documents\module 6 adpro\hello\target\debug\deps\hello-54e026682740f9fb.d: src\main.rs
+
+C:\Users\seanm\OneDrive\Documents\module 6 adpro\hello\target\debug\deps\libhello-54e026682740f9fb.rmeta: src\main.rs
+
+src\main.rs:
---
+++ Projects/hello/target/debug/deps/hello.d
@@ -0,0 +1,5 @@
+C:\Users\seanm\OneDrive\Documents\module 6 adpro\hello\target\debug\deps\hello.d: src\main.rs
+
+C:\Users\seanm\OneDrive\Documents\module 6 adpro\hello\target\debug\deps\hello.exe: src\main.rs
+
+src\main.rs:
